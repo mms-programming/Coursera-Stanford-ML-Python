059aca834b180aa8b02fefa94a177ac2b1cde3a5
mms-programming <mms.programming@gmail.com>
2017-07-27 22:19:45 -0400

    Create solution to computeCost.py

--- ex1/computeCost.py
+++ ex1/computeCost.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def computeCost(X, y, theta):
     """
-       computes the cost of using theta as the parameter for linear 
+       computes the cost of using theta as the parameter for linear
        regression to fit the data points in X and y
     """
     m = y.size
@@ -15,6 +15,14 @@ def computeCost(X, y, theta):
 
 # =========================================================================
 
+    # Switch to np matrices
+    X_mat = np.matrix(X)
+    y_mat = np.matrix(y)
+    theta_mat = np.matrix(theta)
+
+    # X_mat is (97, 2), theta_mat starts off as (1, 2), y starts as (1, 97)
+    h_theta_mat = X_mat * theta_mat.transpose()
+
+    J = sum(np.square(h_theta_mat - y_mat.transpose())) / (2 * m)
+
     return J
-
-
