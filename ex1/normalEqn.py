d7cf81ca90b225f77b948417881d8e940bf4bcc8
Mihai Sirbu <mihai.sirbu@capitalone.com>
2017-08-07 14:31:03 -0400

    Add normalization equation solution

--- ex1/normalEqn.py
+++ ex1/normalEqn.py
@@ -1,12 +1,11 @@
 import numpy as np
-
+from numpy.linalg import inv
 
 def normalEqn(X,y):
     """ Computes the closed-form solution to linear regression
        normalEqn(X,y) computes the closed-form solution to linear
        regression using the normal equations.
     """
-    theta = 0
 # ====================== YOUR CODE HERE ======================
 # Instructions: Complete the code to compute the closed form solution
 #               to linear regression and put the result in theta.
@@ -17,7 +16,11 @@ def normalEqn(X,y):
 
 # -------------------------------------------------------------
 
+    #https://docs.scipy.org/doc/numpy/reference/generated/numpy.linalg.inv.html
+    # Formula theta = inverted(X transpose * X) * (X transpose * y)
+    A = inv(np.dot(X.transpose(), X))
+    theta = np.dot(A, np.dot(X.transpose(), y))
+
     return theta
 
 # ============================================================
-
