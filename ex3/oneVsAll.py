46b890a9b3d70c7c1b89b6a88155eb8546793027
mms-programming <mms.programming@gmail.com>
2017-08-08 06:57:24 -0400

    Add code to do oneVsAll linear regression

--- ex3/oneVsAll.py
+++ ex3/oneVsAll.py
@@ -14,7 +14,7 @@ def oneVsAll(X, y, num_labels, Lambda):
 # Some useful variables
     m, n = X.shape
 
-# You need to return the following variables correctly 
+# You need to return the following variables correctly
     all_theta = np.zeros((num_labels, n + 1))
 
 # Add ones to the X data matrix
@@ -23,11 +23,11 @@ def oneVsAll(X, y, num_labels, Lambda):
 # ====================== YOUR CODE HERE ======================
 # Instructions: You should complete the following code to train num_labels
 #               logistic regression classifiers with regularization
-#               parameter lambda. 
+#               parameter lambda.
 #
 # Hint: theta(:) will return a column vector.
 #
-# Hint: You can use y == c to obtain a vector of 1's and 0's that tell use 
+# Hint: You can use y == c to obtain a vector of 1's and 0's that tell use
 #       whether the ground truth is true/false for this class.
 #
 # Note: For this assignment, we recommend using fmincg to optimize the cost
@@ -37,11 +37,24 @@ def oneVsAll(X, y, num_labels, Lambda):
     # Set Initial theta
     initial_theta = np.zeros((n + 1, 1))
 
-    # This function will return theta and the cost
+    # Hint: See line 82 in ex2.py on how to use the minimize function
+    # Hint: the minimize function requires (R,) shape NOT (R,1)
 
+    # This function will return theta and the cost
+    for i in range(num_labels):
+      z = y == (i + 1)
+      # Note: Not required -- True/False can be used too
+      int_vector = np.vectorize(int)
+      int_vector(z)
+
+      z2 = z.flatten()
+
+      res = minimize(lrCostFunction, initial_theta, method='TNC',
+                               jac=False, args=(X, z2, Lambda),
+                               options={'gtol': 1e-3, 'disp': True, 'maxiter': 1000})
+      all_theta[i,] = res.x
 
 
 # =========================================================================
 
     return all_theta
-
