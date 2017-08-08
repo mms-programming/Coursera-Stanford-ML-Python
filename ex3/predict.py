7a4138878cede937fbfa3558c5cfc9efddde1c9b
mms-programming <mms.programming@gmail.com>
2017-08-08 07:00:45 -0400

    Create simple feed forward neural network predict

--- ex3/predict.py
+++ ex3/predict.py
@@ -13,7 +13,7 @@ def predict(Theta1, Theta2, X):
 
 # ====================== YOUR CODE HERE ======================
 # Instructions: Complete the following code to make predictions using
-#               your learned neural network. You should set p to a 
+#               your learned neural network. You should set p to a
 #               vector containing labels between 1 to num_labels.
 #
 # Hint: The max function might come in useful. In particular, the max
@@ -24,5 +24,13 @@ def predict(Theta1, Theta2, X):
 
 # =========================================================================
 
-    return p + 1        # add 1 to offset index of maximum in A row
+    # https://docs.scipy.org/doc/numpy/reference/generated/numpy.column_stack.html
+    # See also predictOneVsAll.py in ex3
+    X = np.column_stack((np.ones(shape=(m,1)), X))
+    hidden_layer = sigmoid(np.dot(X, Theta1.transpose()))
+    hidden_layer = np.column_stack((np.ones(m), hidden_layer))
 
+    output = sigmoid(np.dot(hidden_layer, Theta2.transpose()))
+    p = np.argmax(output, axis=1)
+
+    return p + 1        # add 1 to offset index of maximum in A row
