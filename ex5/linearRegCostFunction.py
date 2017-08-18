c623a57fd0c0121e3910bbfcb752b11bc032fd70
Mihai Sirbu <mihai.sirbu@capitalone.com>
2017-08-17 23:00:22 -0400

    Complete linear regularized cost function

--- ex5/linearRegCostFunction.py
+++ ex5/linearRegCostFunction.py
@@ -9,7 +9,7 @@ def linearRegCostFunction(X, y, theta, Lambda):
     m = y.size # number of training examples
 
 # ====================== YOUR CODE HERE ======================
-# Instructions: Compute the cost and gradient of regularized linear 
+# Instructions: Compute the cost and gradient of regularized linear
 #               regression for a particular choice of theta.
 #
 #               You should set J to the cost and grad to the gradient.
@@ -18,4 +18,14 @@ def linearRegCostFunction(X, y, theta, Lambda):
 
 # =========================================================================
 
+    h_theta = np.dot(X, theta)
+    regular_cost = sum(np.square(h_theta - y)) / (2 * m )
+    regularization = sum(np.square(theta[1:,])) * Lambda / (2 * m)
+
+    J = regular_cost + regularization
+
+    inside = h_theta - y
+    
+    grad = np.dot(inside, X) / m
+    grad[1:,] = grad[1:,] + Lambda * theta[1:] / m
     return J, grad
