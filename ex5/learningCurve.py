cf99f71b25d70ff27fffa156b9f41b2ae0fa071b
Mihai Sirbu <mihai.sirbu@capitalone.com>
2017-08-17 23:04:59 -0400

    Add learning curve code

--- ex5/learningCurve.py
+++ ex5/learningCurve.py
@@ -23,9 +23,9 @@ def learningCurve(X, y, Xval, yval, Lambda):
     error_val   = np.zeros(m)
 
 # ====================== YOUR CODE HERE ======================
-# Instructions: Fill in this function to return training errors in 
-#               error_train and the cross validation errors in error_val. 
-#               i.e., error_train(i) and 
+# Instructions: Fill in this function to return training errors in
+#               error_train and the cross validation errors in error_val.
+#               i.e., error_train(i) and
 #               error_val(i) should give you the errors
 #               obtained after training on i examples.
 #
@@ -36,24 +36,28 @@ def learningCurve(X, y, Xval, yval, Lambda):
 #       the _entire_ cross validation set (Xval and yval).
 #
 # Note: If you are using your cost function (linearRegCostFunction)
-#       to compute the training and cross validation error, you should 
-#       call the function with the lambda argument set to 0. 
+#       to compute the training and cross validation error, you should
+#       call the function with the lambda argument set to 0.
 #       Do note that you will still need to use lambda when running
 #       the training to obtain the theta parameters.
 #
 # Hint: You can loop over the examples with the following:
 #
 #       for i = 1:m
-#           # Compute train/cross validation errors using training examples 
-#           # X(1:i, :) and y(1:i), storing the result in 
+#           # Compute train/cross validation errors using training examples
+#           # X(1:i, :) and y(1:i), storing the result in
 #           # error_train(i) and error_val(i)
 #           ....
-#           
+#
 #       end
 #
 
 # ---------------------- Sample Solution ----------------------
 
+    for i in range(m):
+      theta = trainLinearReg(X[0:i+1], y[0:i+1], Lambda)
+      error_train[i] = linearRegCostFunction(X[0:i+1], y[0:i+1], theta, 0)[0]
+      error_val[i] = linearRegCostFunction(Xval, yval, theta, 0)[0]
 
 
 # -------------------------------------------------------------------------
